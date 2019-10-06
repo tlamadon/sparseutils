5a2466fc08998d69a1783219994e6c3db47784a8
Thibaut Lamadon <thibaut.lamadon@gmail.com>
2019-10-06 12:53:45 -0500

    - added tests

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(sparseutils)
+
+test_check("sparseutils")
---
+++ tests/testthat/test_functions.R
@@ -0,0 +1,26 @@
+context("Sparse methods")
+library(sparseutils)
+library(testthat)
+library(Matrix)
+
+test_that("testing the sparse functions", {
+
+  # create a dense matrix
+  M = array(runif(5*5),c(5,5))
+  A = Matrix::t(1.0*as(array( (runif(10*5)>0.7)*rnorm(10*5) ,c(10,5)), "sparseMatrix"))
+  B = Matrix::t(1.0*as(array( (runif(10*5)>0.7)*rnorm(10*5) ,c(10,5)), "sparseMatrix"))
+  r0 = sparseDiagCross(A,M,B,dim(A)[2])
+  r1 = Matrix::diag(Matrix::t(A) %*% M %*% B)
+  expect_true( mean( (r0-r1)^2 ) < 1e-6)
+
+  # testing a second type of function
+  A = Matrix::t(1.0*as(array( (runif(10*5)>0.7)*rnorm(10*5) ,c(10,5)), "sparseMatrix"))
+  B = Matrix::t(1.0*as(array( (runif(10*5)>0.7)*rnorm(10*5) ,c(10,5)), "sparseMatrix"))
+  Q = Matrix::t(1.0*as(array( (runif(5*5)>0.7)*rnorm(5*5) ,c(5,5)), "sparseMatrix"))
+  M = array(runif(5*5),c(5,5))
+  M = .5* (M + t(M))
+  
+  r0 = Matrix::diag(Matrix::t(A) %*% M %*% Q %*% M %*% B)
+  r1 = sparseSandwichTrace(A, M, Q, B, 10, 5)
+  expect_true( mean( (r0-r1)^2 ) < 1e-6)
+})
